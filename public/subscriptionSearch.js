897814ad96b31381cbe9dd51f19c0db18b184400
sacurran <sacurran@umich.edu>
2015-05-13 09:35:49 -0500

    Learning about async

--- public/subscriptionSearch.js
+++ public/subscriptionSearch.js
@@ -1,11 +1,8 @@
+
+
 // After the API loads, call a function to enable the search box.
 function handleAPILoaded() {
   $('#search-button').attr('disabled', false);
-  gapi.auth.authorize({
-    client_id: OAUTH2_CLIENT_ID,
-    scope: OAUTH2_SCOPES,
-    immediate: true
-  }, getChannelsForUser);
 }
 
 // Search for a specified string.
@@ -18,11 +15,64 @@ function search() {
 
   request.execute(function(response) {
     var responseString = JSON.stringify(response, '', 2);
+    
     $('#search-container').html('<pre>' + responseString + '</pre>');
   });
 }
 
-function getChannelsForUser(user) {
-    var url = baseurl + '/channels?part=id&forUsername=StephanieAnnaCurran';
-    console.log(url);
+function getSubs(callback) {
+    var request =gapi.client.youtube.subscriptions.list({
+       part: 'snippet',
+       mine: true,
+       maxResults: 50,
+    });
+    request.execute(function(response) {
+    var channelIds= new Array();
+    var responseString = JSON.stringify(response, '', 2);
+    var length = Object.keys(response.items).length;
+    for(var i=0;i<length;i++)
+    {
+        channelIds.push(response.items[i].snippet.resourceId.channelId);
+    }
+    console.log(channelIds.length);
+    $('#search-container').html('<pre>' + responseString + '</pre>');
+    callback(channelIds);
+  });
+}
+
+function addVideosFromChannel(videoIds, channelId)
+{
+    console.log("The channel we used: "+ channelId);
+    var request = gapi.client.youtube.search.list({
+       part: 'snippet',
+       channelId: channelId,
+       maxResults: 50
+    });
+
+    
+    request.execute(function(response) {
+    var length = Object.keys(response.items).length;
+    for(var i=0;i<length;i++)
+    {
+        console.log()
+    }   
+        
+        
+    var responseString = JSON.stringify(response, '', 2);
+    var length = Object.keys(response.items).length;
+   $('#search-container').html('<pre>' + responseString + '</pre>');
+  });
+}
+
+
+function getVideosForTopChannels(ids)
+{
+    videoIds = [];
+    addVideosFromChannel(videoIds,ids[0]);
+    
+}
+
+function main()
+{
+    getSubs(getVideosForTopChannels);
 }
